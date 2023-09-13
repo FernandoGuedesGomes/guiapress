a18c1577426aff44727d1d360daf158f059b0ab4
Fernando <fernando.direitosnt@gmail.com>
2023-09-13 16:11:13 -0300

    Configurado form Post

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-router.get("/categories", (req, res)=>{
-    res.send("ROTA DE CATEGORAS.");
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res)=>{
-    res.send("ROTA PARA CRIAR UMA NOVA CATEGORIA.");
+    res.render("admin/categories/new")
 });
+
+router.post("/categories/save", (req, res)=> {
+    var title = req.body.title;
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/");
+        })
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+})
 
 module.exports = router;
