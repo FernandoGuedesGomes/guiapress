535b59c1eaf9a3c30c7153530ba12363e824d39d
Fernando <fernando.direitosnt@gmail.com>
2023-10-07 16:22:59 -0300

    add router admin/articles/edit

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,56 +4,69 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugfy = require("slugify");
 
-
-router.get("/admin/articles", (req, res)=>{
-    Article.findAll({
-        include: [{model: Category }]
-    }).then(articles => {
-        res.render("admin/articles/index", {articles: articles});
-    });
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
-router.get("/admin/articles/new",(req, res)=>{
-    Category.findAll().then(categories =>{
-        res.render("admin/articles/new",{categories: categories});
-    })
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
 });
 
-router.post("/articles/save", (req, res)=>{
-    var title = req.body.title;
-    var body = req.body.body;
-    var category = req.body.category;
+router.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
 
-    Article.create({
-        title: title,
-        slug: slugfy(title),
-        body: body,
-        categoryId: category
-    }).then(() => {
-        res.redirect("/admin/articles")
-    })
-
+  Article.create({
+    title: title,
+    slug: slugfy(title),
+    body: body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
 
 router.post("/articles/delete", (req, res) => {
-    var id = req.body.id;
-    if (id != undefined) {
-        if (!isNaN(id)) {
-            Article.destroy({
-                where: {
-                    id: id
-                }
-            }).then(() => {
-                res.redirect("/admin/articles");
-            })
-
-        } else {
-            res.redirect("/admin/articles");
-        }
-
-    } else {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
         res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
     }
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id;
+  Article.findByPk(id)
+    .then((article) => {
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render("admin/articles/edit", { categories: categories });
+        });
+      } else {
+        res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
 });
 
 module.exports = router;
